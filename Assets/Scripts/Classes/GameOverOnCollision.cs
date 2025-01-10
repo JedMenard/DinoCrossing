c678334946079f46af58fc19cfe5d5177c51441b
Jed Menard <jedm0524@gmail.com>
2025-01-09 18:35:54 -0800

    Connected game over scene

---
+++ Assets/Scripts/Classes/GameOverOnCollision.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameOverOnCollision : MonoBehaviour
+{
+    [SerializeField]
+    private float gameOverDelay = 2;
+
+    private LevelManager levelManager;
+
+    private void Awake()
+    {
+        this.levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) => this.ProcessCollision(collision.gameObject);
+
+    private void OnCollisionEnter2D(Collision2D collision) => this.ProcessCollision(collision.gameObject);
+
+    private void ProcessCollision(GameObject collision)
+    {
+        if (collision.TryGetComponent<PlayerInput>(out _))
+        {
+            // Colliding with player.
+            // Destroy the player and trigger game over.
+            Destroy(collision);
+            this.levelManager.LoadGameOver(this.gameOverDelay);
+        }
+    }
+}
